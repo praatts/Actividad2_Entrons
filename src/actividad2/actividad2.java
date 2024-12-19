83ec75e569f5bed8813033f2ba5fa7938e37ba5e
DaniMP84 <xDani84x@gmail.com>
2024-12-19 15:44:41 +0100

    matriz inicializada, procedo a meter el random dentro

--- src/actividad2/actividad2.java
+++ src/actividad2/actividad2.java
@@ -5,10 +5,10 @@ public class actividad2 {
 	public static void main(String[] args) {
 	 
 		
-		int[][] matriz_inicial;
+		
 		final int COLUMNAS = 5;
 		final int FILAS = 5;
-		
+		int[][] matriz = new int [FILAS][COLUMNAS];
 		Random nums = new Random(101);
 		
 		
