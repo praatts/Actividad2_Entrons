1c8980d92b361283bb28681c10b1fc803d07dae8
DaniMP84 <xDani84x@gmail.com>
2024-12-19 15:49:22 +0100

    Bucle for que guarda los números randoms en la matriz.

--- src/actividad2/actividad2.java
+++ src/actividad2/actividad2.java
@@ -1,17 +1,23 @@
 package actividad2;
+
 import java.util.*;
+
 public class actividad2 {
 
 	public static void main(String[] args) {
-	 
-		
-		
+
 		final int COLUMNAS = 5;
 		final int FILAS = 5;
-		int[][] matriz = new int [FILAS][COLUMNAS];
+		int[][] matriz = new int[FILAS][COLUMNAS];
 		Random nums = new Random(101);
 		
 		
+		//bucle que recorre la matriz y lo rellena con el random.
+		for (int i = 0; i < FILAS; i++) {
+			for (int j = 0; j < COLUMNAS; j++) {
+				matriz[i][j] = nums.nextInt(101);
+			}
+		}
 
 	}
 
