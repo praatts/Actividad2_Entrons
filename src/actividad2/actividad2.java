170616e7bc52ee3993a9970069131920a9422b84
𝑱𝒂𝒏 𝑸𝒖𝒆𝒓𝒂𝒍𝒕 <55952925+jaanque@users.noreply.github.com>
2024-12-19 16:40:57 +0100

    Update actividad2.java

--- src/actividad2/actividad2.java
+++ src/actividad2/actividad2.java
@@ -1,10 +1,24 @@
 package actividad2;
 
+import java.util.*;
+
 public class actividad2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		// Sumar todos los elementos de la matriz.
+		int suma = 0;
+		for (int i = 0; i < FILAS; i++) {
+		    for (int j = 0; j < COLUMNAS; j++) {
+		        suma += matriz[i][j];
+		    }
+		}
 
+		// Mostrar la suma de todos los elementos.
+		System.out.println("La suma de todos los elementos de la matriz es: " + suma);
+
+		
 	}
 
 }
