d344de17020e1f93876fb20b448031e47bb2c679
praatts <127567514+praatts@users.noreply.github.com>
2024-12-19 16:01:09 +0100

    Pide al usuario número y comrpueba si está en la matriz

--- src/actividad2/actividad2.java
+++ src/actividad2/actividad2.java
@@ -1,10 +1,25 @@
 package actividad2;
-
+import java.util.Scanner;
 public class actividad2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		Scanner s = new Scanner(System.in);
+		
+		//Pide un número al usuario que quiera comprobar si está en la matriz.
+		
+		System.out.println("Introduce el número que quieres comprobar si está en la matriz");
+		int numABuscar = s.nextInt();
+		
+		for (int i = 0; i < matriz.length; i++) {
+			for (int j = 0; j < matriz[i].length; j++) {
+				if (numABuscar == matriz[i][j]) {
+					System.out.println("Se ha encontrado el número en la matriz!");
+				} 
+			}
+		}
+		
 	}
 
 }
