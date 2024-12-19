313cc45e3248f3baa962a0105beaf586a0e8f377
DaniMP84 <xDani84x@gmail.com>
2024-12-19 15:51:19 +0100

    He creado la muestra por pantalla de la matriz con los numeros randoms añadidos.

--- src/actividad2/actividad2.java
+++ src/actividad2/actividad2.java
@@ -10,15 +10,21 @@ public class actividad2 {
 		final int FILAS = 5;
 		int[][] matriz = new int[FILAS][COLUMNAS];
 		Random nums = new Random(101);
-		
-		
-		//bucle que recorre la matriz y lo rellena con el random.
+
+		// bucle que recorre la matriz y lo rellena con el random.
 		for (int i = 0; i < FILAS; i++) {
 			for (int j = 0; j < COLUMNAS; j++) {
 				matriz[i][j] = nums.nextInt(101);
 			}
 		}
 
+		//Imprimo el bucle y lo muestro por pantalla.
+		for (int i = 0; i < FILAS; i++) {
+			for (int j = 0; j < COLUMNAS; j++) {
+				System.out.print(matriz[i][j] + " ");
+			}
+			System.out.println();
+		}
 	}
 
 }
